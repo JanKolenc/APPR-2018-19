2e0314a1804e63965073e432d0d74da718f4c29c
Jan Kolenc <ostrostrelec.jan@gmail.com>
2018-12-21 07:26:25 +0100

    Testiranje tidy

--- uvoz/Tralala.R
+++ uvoz/Tralala.R
@@ -1,14 +1,40 @@
 library(readr)
+library(reshape2)
+library(dplyr)
+
+sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
+sl <- locale(encoding = "Windows-1250")
 
 izobrazba <- read.csv2("izobrazba.csv", col.names=c("1","Starost", "Izobrazba", "Spol",2008:2017),
-                na="-",dec = ".") 
+                na="-",dec = ".", header = FALSE) 
 izobrazba <- izobrazba[,-1]
 
+#========================================================================================================
+#TABELA1 - Izobrazba 16+ let
 
-prag <- read_csv2("prag.csv", col.names=c("1","Valuta", "Tip gospodinjstva",2005:2017),
+izobrazba16m <- izobrazba[c(1, 3, 5,7),-1]
+
+izobrazba16m.stopnja <- melt(izobrazba16m, id.vars="Izobrazba", measure.vars=names(izobrazba16m)[-1],
+                    variable.name="Leto", value.name="Odstotki",
+                    na.rm=TRUE)
+izobrazba16m.stopnja <- izobrazba16m.stopnja[5:44,]
+
+izobrazba16z <- izobrazba[c(2, 4, 6,8),-1]
+
+izobrazba16z.stopnja <- melt(izobrazba16z, id.vars="Izobrazba", measure.vars=names(izobrazba16z)[-1],
+                             variable.name="Leto", value.name="Odstotki",
+                             na.rm=TRUE)
+izobrazba16z.stopnja <- izobrazba16z.stopnja[5:44,]
+
+
+#========================================================================================================
+#TABELA2 - Izobrazba 18+ let
+
+prag <- read_csv2("prag.csv", col.names=c("1","Valuta", "Tip gospodinjstva", 2005:2017),
                        na="-",dec = ".") 
 prag <- prag[,-1]
 
 starost <- read.csv2("starost.csv", col.names=c("1","Starost", "Spol",2005:2017),
                        na="-",dec = ".",encoding = "UTF-8") 
 starost <- starost[,-1]
+
