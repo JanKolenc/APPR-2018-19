ee962925c899fe6f21dce752f850ecee202c38b7
Jan Kolenc <ostrostrelec.jan@gmail.com>
2019-01-16 18:23:07 +0100

    try

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -158,14 +158,9 @@ colnames(I2017) <- c("Starost","Izobrazba","Spol","2017")
 
 #========================================================================================================
 #TABELA15 - Prag
+prag <- read_csv2("prag.csv")
+colnames(prag) <- c("1","Valuta", "Tip gospodinjstva", 2005:2017)
 
-prag <- read_csv2("prag.csv", col.names=c("1","Valuta", "Tip gospodinjstva", 2005:2017),
-                       na="-",dec = ".") 
-prag <- prag[,-1]
-
-starost <- read.csv2("starost.csv", col.names=c("1","Starost", "Spol",2005:2017),
-                       na="-",dec = ".",encoding = "UTF-8") 
-starost <- starost[,-1]
 
 
 
