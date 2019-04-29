468c44eb8ebf37048a9ddc3e5562034ae4b84f06
Jan Kolenc <ostrostrelec.jan@gmail.com>
2019-04-29 16:53:33 +0200

    1

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -9,4 +9,8 @@ library(plotly)
 
 source(file = 'lib/uvozi.zemljevid.r', encoding = 'UTF-8')
 source('lib/libraries.r', encoding = 'UTF-8')
-source('uvoz/uvoz_test.r', encoding = 'UTF-8')
+source('uvoz/uvoz.r', encoding = 'UTF-8')
+
+Slovenija <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
+                             "SVN_adm1") %>% fortify()
+colnames(Slovenija)[12]<-'Regija'
