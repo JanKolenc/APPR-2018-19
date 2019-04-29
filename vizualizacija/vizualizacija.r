76e8692801cc293ce079549308c45cd0278c7732
Jan Kolenc <ostrostrelec.jan@gmail.com>
2019-04-29 19:56:45 +0200

    Dodal sem vse po letih

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -37,15 +37,113 @@ regije.2016 <- filter(regije1, Leto == '2016')
 regije.2017 <- filter(regije1, Leto == '2017')
 
 regije.2008 <- regije.2008[c(-2)]
+regije.2009 <- regije.2009[c(-2)]
+regije.2010 <- regije.2010[c(-2)]
+regije.2011 <- regije.2011[c(-2)]
+regije.2012 <- regije.2012[c(-2)]
+regije.2013 <- regije.2013[c(-2)]
+regije.2014 <- regije.2014[c(-2)]
+regije.2015 <- regije.2015[c(-2)]
+regije.2016 <- regije.2016[c(-2)]
+regije.2017 <- regije.2017[c(-2)]
 
 
 zemljevid.2008.regije <- ggplot() +
   geom_polygon(data = anti_join(regije.2008,Slovenija, by = c('Regija')),
                aes(x = long, y = lat, group = group, fill= regije1))+
-  xlab("") + ylab("") + ggtitle('Brezposelnost v letu 2007') + 
+  xlab("") + ylab("") + ggtitle('Regije 2008') + 
   theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
   scale_fill_gradient(low = '#ffb3b3', high='#660000')
-zemljevid.2008.regije$labels$fill <- 'Stopnja registrirane brezposelnosti'
+zemljevid.2008.regije$labels$fill <- 'Regije 2008'
 
+#========================================================================================================
 
+zemljevid.2009.regije <- ggplot() +
+  geom_polygon(data = anti_join(regije.2009,Slovenija, by = c('Regija')),
+               aes(x = long, y = lat, group = group, fill= regije1))+
+  xlab("") + ylab("") + ggtitle('Regije 2009') + 
+  theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
+  scale_fill_gradient(low = '#ffb3b3', high='#660000')
+zemljevid.2009.regije$labels$fill <- 'Regije 2009'
 
+#========================================================================================================
+
+zemljevid.2010.regije <- ggplot() +
+  geom_polygon(data = anti_join(regije.2010,Slovenija, by = c('Regija')),
+               aes(x = long, y = lat, group = group, fill= regije1))+
+  xlab("") + ylab("") + ggtitle('Regije 2010') + 
+  theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
+  scale_fill_gradient(low = '#ffb3b3', high='#660000')
+zemljevid.2010.regije$labels$fill <- 'Regije 2010'
+
+#========================================================================================================
+
+zemljevid.2011.regije <- ggplot() +
+  geom_polygon(data = anti_join(regije.2011,Slovenija, by = c('Regija')),
+               aes(x = long, y = lat, group = group, fill= regije1))+
+  xlab("") + ylab("") + ggtitle('Regije 2011') + 
+  theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
+  scale_fill_gradient(low = '#ffb3b3', high='#660000')
+zemljevid.2011.regije$labels$fill <- 'Regije 2011'
+
+#========================================================================================================
+
+zemljevid.2012.regije <- ggplot() +
+  geom_polygon(data = anti_join(regije.2012,Slovenija, by = c('Regija')),
+               aes(x = long, y = lat, group = group, fill= regije1))+
+  xlab("") + ylab("") + ggtitle('Regije 2012') + 
+  theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
+  scale_fill_gradient(low = '#ffb3b3', high='#660000')
+zemljevid.2012.regije$labels$fill <- 'Regije 2012'
+
+#========================================================================================================
+
+zemljevid.2013.regije <- ggplot() +
+  geom_polygon(data = anti_join(regije.2013,Slovenija, by = c('Regija')),
+               aes(x = long, y = lat, group = group, fill= regije1))+
+  xlab("") + ylab("") + ggtitle('Regije 2013') + 
+  theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
+  scale_fill_gradient(low = '#ffb3b3', high='#660000')
+zemljevid.2013.regije$labels$fill <- 'Regije 2013'
+
+#========================================================================================================
+
+zemljevid.2014.regije <- ggplot() +
+  geom_polygon(data = anti_join(regije.2014,Slovenija, by = c('Regija')),
+               aes(x = long, y = lat, group = group, fill= regije1))+
+  xlab("") + ylab("") + ggtitle('Regije 2014') + 
+  theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
+  scale_fill_gradient(low = '#ffb3b3', high='#660000')
+zemljevid.2014.regije$labels$fill <- 'Regije 2014'
+
+#========================================================================================================
+
+zemljevid.2015.regije <- ggplot() +
+  geom_polygon(data = anti_join(regije.2015,Slovenija, by = c('Regija')),
+               aes(x = long, y = lat, group = group, fill= regije1))+
+  xlab("") + ylab("") + ggtitle('Regije 2015') + 
+  theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
+  scale_fill_gradient(low = '#ffb3b3', high='#660000')
+zemljevid.2015.regije$labels$fill <- 'Regije 2015'
+
+#========================================================================================================
+
+zemljevid.2016.regije <- ggplot() +
+  geom_polygon(data = anti_join(regije.2016,Slovenija, by = c('Regija')),
+               aes(x = long, y = lat, group = group, fill= regije1))+
+  xlab("") + ylab("") + ggtitle('Regije 2016') + 
+  theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
+  scale_fill_gradient(low = '#ffb3b3', high='#660000')
+zemljevid.2016.regije$labels$fill <- 'Regije 2016'
+
+#========================================================================================================
+
+zemljevid.2017.regije <- ggplot() +
+  geom_polygon(data = anti_join(regije.2017,Slovenija, by = c('Regija')),
+               aes(x = long, y = lat, group = group, fill= regije1))+
+  xlab("") + ylab("") + ggtitle('Regije 2017') + 
+  theme(axis.title=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), panel.background = element_blank()) + 
+  scale_fill_gradient(low = '#ffb3b3', high='#660000')
+zemljevid.2017.regije$labels$fill <- 'Regije 2017'
+
+#========================================================================================================
